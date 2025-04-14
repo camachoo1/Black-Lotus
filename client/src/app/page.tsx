76daa0c615deea6f3e8ec4847e123f28a29e76ff
camachoo1 <ocamacho812@gmail.com>
2025-04-13 19:16:57 -0700

    Starting initial skeleton of landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,25 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
-        Welcome to Black Lotus
-      </main>
-    </div>
+    <nav className='flex justify-between'>
+      {/* LEFT SIDE OF NAVBAR */}
+      <div className='flex items-center'>
+        <Image
+          src='/filled.svg'
+          alt='logo'
+          height={100}
+          width={100}
+        />
+        <h1 className='flex font-bold text-2xl'>Black Lotus</h1>
+      </div>
+      {/* RIGHT SIDE OF NAVBAR */}
+      <div className='flex items-center'>
+        <div className='flex space-x-3'>
+          <button className=''>Login</button>
+          <button className=''>Sign Up</button>
+        </div>
+      </div>
+    </nav>
   );
 }
