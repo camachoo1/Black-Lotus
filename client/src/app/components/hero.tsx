5304ecc64eddfc62dd515e61ed22b5be456691e3
camachoo1 <ocamacho812@gmail.com>
2025-04-15 01:07:00 -0700

    Add some fade in animations with framer motion

--- client/src/app/components/hero.tsx
+++ client/src/app/components/hero.tsx
@@ -1,22 +1,41 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
 export function Hero() {
   return (
     // MAIN CONTAINER
-    <div className='flex py-24 px-4 justify-center items-center'>
+    <div className='flex flex-col py-24 px-4 justify-center items-center'>
       {/* INITIAL CONTAINER FOR INTRO */}
       <div className='text-center max-w-4xl'>
-        <h1 className='text-3xl custom-text font-bold p-2'>
+        <motion.h1
+          className='text-3xl custom-text font-bold p-2'
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.2, duration: 1.5 }}
+        >
           That trip you always talked about with your friends but
           never happened just got easier to plan
-        </h1>
-        <h3 className='text-gray-500 custom-text text-sm p-2'>
+        </motion.h1>
+        <motion.h3
+          className='text-gray-500 custom-text text-sm p-2'
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1, duration: 1 }}
+        >
           Create detailed itineraries, manage all your bookings, find
           great events in the areas your visiting, and share your trip
           with your friends from this one app!
-        </h3>
+        </motion.h3>
 
-        <button className='custom-text text-lg focus:outline-none bg-cyan-400 hover:bg-cyan-500 rounded-full focus:ring-4 px-8 py-3.5 me-2 mb-2 font-bold text-white'>
+        <motion.button
+          className='hover:cursor-pointer custom-text text-lg focus:outline-none bg-cyan-400 hover:bg-cyan-500 rounded-full focus:ring-4 px-8 py-3.5 me-2 mb-2 font-bold text-white'
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1.5, duration: 1 }}
+        >
           Start Planning
-        </button>
+        </motion.button>
       </div>
     </div>
   );
--- client/src/app/components/navbar.tsx
+++ client/src/app/components/navbar.tsx
@@ -5,21 +5,18 @@ export function Navbar() {
     <nav className='flex justify-around'>
       {/* LEFT SIDE OF NAVBAR */}
       <div className='flex items-center'>
-        <Image
-          src='/filled.svg'
-          alt='logo'
-          height={75}
-          width={75}
-        />
-        <h1 className='flex custom-text font-bold text-lg'>Black Lotus</h1>
+        <Image src='/filled.svg' alt='logo' height={75} width={75} />
+        <h1 className='flex custom-text font-bold text-lg'>
+          Black Lotus
+        </h1>
       </div>
       {/* RIGHT SIDE OF NAVBAR */}
       <div className='flex items-center'>
         <div className='flex px-3 m-2 space-x-2'>
-          <button className='custom-text text-gray-900 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-semibold rounded-lg text-xs px-5 py-2.5 me-2 mb-2'>
+          <button className='custom-text hover:cursor-pointer text-gray-900 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-semibold rounded-lg text-xs px-5 py-2.5 me-2 mb-2'>
             Login
           </button>
-          <button className='custom-text focus:outline-none text-white bg-cyan-400 hover:bg-cyan-500 focus:ring-4 focus:ring-cyan-300 font-semibold rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:focus:ring-cyan-900'>
+          <button className='custom-text hover:cursor-pointer focus:outline-none text-white bg-cyan-400 hover:bg-cyan-500 focus:ring-4 focus:ring-cyan-300 font-semibold rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:focus:ring-cyan-900'>
             Sign Up
           </button>
         </div>
