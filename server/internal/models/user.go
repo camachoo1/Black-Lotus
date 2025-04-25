6deaf128da157f0fe3e4054c9f6147d00846494f
camachoo1 <ocamacho812@gmail.com>
2025-04-24 19:30:09 -0700

    Add user model

---
+++ server/internal/models/user.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID            uuid.UUID  `json:"id"`
+	Name          string     `json:"name"`
+	Email         string     `json:"email"`
+	HashedPassword *string    `json:"hashed_password,omitempty"`
+	EmailVerified  bool       `json:"email_verified" default:"false"`
+	CreatedAt     time.Time   `json:"created_at"`
+	UpdatedAt     time.Time   `json:"updated_at"`
+}
+
+type CreateUserInput struct {
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password *string `json:"password" validate:"omitempty,min=8"`
+}
