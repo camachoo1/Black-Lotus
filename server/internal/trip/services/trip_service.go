e39242d7a28124ee5ab6259df4a675bc8b5e08e5
camachoo1 <ocamacho812@gmail.com>
2025-05-01 13:29:34 -0700

    Add trip service for trip creation

---
+++ server/internal/trip/services/trip_service.go
@@ -0,0 +1,41 @@
+package services
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+
+	"black-lotus/internal/trip/models"
+	"black-lotus/internal/trip/repositories"
+)
+
+type TripService struct {
+	tripRepo *repositories.TripRepository
+}
+
+func NewTripService(tripRepo *repositories.TripRepository) *TripService {
+	return &TripService{tripRepo: tripRepo}
+}
+
+func (s *TripService) CreateTrip(ctx context.Context, userID uuid.UUID, input models.CreateTripInput) (*models.Trip, error) {
+	// Validate dates from user
+	if input.EndDate.Before(input.StartDate) {
+		return nil, errors.New("end date cannot be before start date")
+	}
+
+	// If name is empty, we generate a default name for the Trip
+	if input.Name == "" {
+		input.Name = fmt.Sprintf("Trip to %s", input.Destination)
+	}
+
+	// Create the trip in the DB
+	trip, err := s.tripRepo.CreateTrip(ctx, userID, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return trip, nil
+}
