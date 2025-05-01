6305f1238df481aeb57fcb305d514ad0442d3778
camachoo1 <ocamacho812@gmail.com>
2025-04-30 20:43:57 -0700

    Add model for trips

---
+++ server/internal/plans/models/plan_model.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Trip struct {
+    ID          uuid.UUID     `json:"id"`
+    UserID      uuid.UUID     `json:"user_id"`
+		// Will generate default names for Trips in service file
+    Name        string        `json:"name"`
+    Description string        `json:"description"`
+    StartDate   time.Time     `json:"start_date" validate:"required"`
+    EndDate     time.Time     `json:"end_date" validate:"required"`
+    Destination string        `json:"destination" validate:"required"`
+    CreatedAt   time.Time     `json:"created_at"`
+    UpdatedAt   time.Time     `json:"updated_at"`
+
+		// WILL NEED TO ADD RELATIONS AS FEATURES GET IMPLEMENTED
+}
+
+type CreateTripInput struct {
+		// Will generate default names for Trips in service file
+    Name        string    `json:"name"`
+    Description string    `json:"description"`
+    StartDate   time.Time `json:"start_date" validate:"required"`
+    EndDate     time.Time `json:"end_date" validate:"required"`
+    Destination string    `json:"destination" validate:"required"`
+}
