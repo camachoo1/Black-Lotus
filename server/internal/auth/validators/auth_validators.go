b457ebd6e8544200a8028e33066379bd7b5e1a8a
camachoo1 <ocamacho812@gmail.com>
2025-05-02 20:21:00 -0700

    Added test file for auth validators

--- server/internal/auth/validators/auth_validators.go
+++ server/internal/auth/validators/auth_validators.go
@@ -17,23 +17,43 @@ func RegisterCustomValidators(v *validator.Validate) {
 // ValidateContainsUppercase checks if the field contains at least one uppercase letter
 func ValidateContainsUppercase(fl validator.FieldLevel) bool {
 	value := fl.Field().String()
+
+	if value == "" {
+		return false
+	}
+
 	return regexp.MustCompile(`[A-Z]`).MatchString(value)
 }
 
 // ValidateContainsLowercase checks if the field contains at least one lowercase letter
 func ValidateContainsLowercase(fl validator.FieldLevel) bool {
 	value := fl.Field().String()
+
+	if value == "" {
+		return false
+	}
+
 	return regexp.MustCompile(`[a-z]`).MatchString(value)
 }
 
 // ValidateContainsNumber checks if the field contains at least one number
 func ValidateContainsNumber(fl validator.FieldLevel) bool {
 	value := fl.Field().String()
+
+	if value == "" {
+		return false
+	}
+
 	return regexp.MustCompile(`[0-9]`).MatchString(value)
 }
 
 // ValidateContainsSpecialChar checks if the field contains at least one special character
 func ValidateContainsSpecialChar(fl validator.FieldLevel) bool {
 	value := fl.Field().String()
+
+	if value == "" {
+		return false
+	}
+
 	return regexp.MustCompile(`[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]`).MatchString(value)
 }
